0bff73089d7eac3d66c570acd68223688e8a1600
Shams-Joha <shamsjoha3@gmail.com>
2024-09-18 23:41:56 +0600

    Finished Home Section

--- js/login.js
+++ js/login.js
@@ -1,16 +1,17 @@
 document.getElementById('btn-login')
     .addEventListener('click', function (event) {
         event.preventDefault();
-        console.log('login Button Clicked');
+        
         const mobile = document.getElementById('in-mobile').value;
       
         const pin = document.getElementById('in-pass').value;
-        
+
         if (mobile === '01312321303' && pin === '1710') {
             console.log('Logged In');
+            window.location.href = "../home.html";
         }
         else{
-            console.log('Wrong Credentials');
+            alert('Wrong credentials')
         }
 
 
